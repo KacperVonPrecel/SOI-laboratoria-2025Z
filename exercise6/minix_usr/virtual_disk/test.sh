e5f02f76f83ebcffed30772d1d5df329dab6616c
Kacper Skrodzki <kacskrogry@gmail.com>
2026-01-07 22:23:25 +0100

    reorganizing file placement

--- exercise6/minix_usr/virtual_disk/test.sh
+++ exercise6/minix_usr/virtual_disk/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Compilation..."
+# Dodajemy sprawdzenie błędów - jeśli kompilacja padnie, skrypt staje.
 cc fs.c -o fs
 if [ $? -ne 0 ]; then echo "Error compiling fs.c"; exit 1; fi
 
@@ -12,7 +13,7 @@ echo "TESTING SCENARIO FOR FILE SYSTEM"
 echo "==========================================="
 
 # 1. Creation of the environment
-echo "\n[1] Creating virtual disk..."
+echo "[1] Creating virtual disk..."
 ./fs -c
 
 # Generating test files
@@ -37,7 +38,7 @@ echo "Hello" > fileE
 sleep 2
 
 # 2. Saving files
-echo "\n[2] Saving files A, B, C onto virtual disk..."
+echo "[2] Saving files A, B, C onto virtual disk..."
 ./fs -cp fileA plik_A
 ./fs -cp fileB plik_B
 ./fs -cp fileC plik_C
@@ -47,35 +48,44 @@ echo "\n[2] Saving files A, B, C onto virtual disk..."
 
 sleep 3
 
-# 3. Fragmentation demonstration
-echo "\n[3] Removing middle file (B) to create a 'hole'..."
+# 3. Trying to copy to disk the file that already exists in disk 
+echo "[3] Saving file 'maly_plik' onto virtual disk..."
+echo "	  Expecting failure due to already exisiting file with the same name"
+./fs -cp fileE maly_plik
+
+./fs -map
+
+sleep 3
+
+# 4. Fragmentation demonstration
+echo "[4] Removing middle file (B) to create a 'hole'..."
 ./fs -rm plik_B
 ./fs -map
 
 sleep 3
 
-echo "\n[4] Trying to save file D (25KB) in hole left after B (20KB)..."
+echo "[5] Trying to save file D (25KB) in hole left after B (20KB)..."
 echo "    Expected result: Failure (even though total space is enough, the hole is too small)."
 ./fs -cp fileD plik_D
 
 sleep 3
 
-echo "\n[5] Demonstrating internal fragmentation..."
+echo "[6] Demonstrating internal fragmentation..."
 echo "    File 'maly_plik' has only a few bytes, but takes up whole 1024B block."
 ./fs -map
 
 sleep 3
 
-# 4. Copying back
-echo "\n[6] Copying 'plik_A' from virtual disk to local (as 'odzyskany_A')..."
+# 5. Copying back
+echo "[7] Copying 'plik_A' from virtual disk to local (as 'odzyskany_A')..."
 ./fs -out plik_A odzyskany_A
-
+# Wyświetlenie szczegółów plików, żeby porównać rozmiary
 ls -l fileA odzyskany_A
 
 sleep 2
 
 # Cleaning up
-echo "\n[7] Cleaning up..."
+echo "[8] Cleaning up..."
 ./fs -rmdisk
 rm fs gen fileA fileB fileC fileD fileE odzyskany_A
 echo "Test completed."
